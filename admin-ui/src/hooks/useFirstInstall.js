f88acc4349e3c04a5db3baa58a37648ac7e9b166
aditisingh2310 <singh.riya20062010@gmail.com>
2026-03-22 12:37:16 +0530

    Create useFirstInstall.js

---
+++ admin-ui/src/hooks/useFirstInstall.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function useFirstInstall(hasProducts) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!hasProducts) {
+      navigate("/onboarding");
+    }
+  }, [hasProducts]);
+}
